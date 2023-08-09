b9dc79fd9bcc7883553997aa9551bb8df48e77ea
Kalyn Stricklin <kalynstricklin@botts-inc.com>
2023-08-09 10:33:47 -0500

    removed overrides in outputs

--- sensors/video/sensorhub-driver-onvif/src/main/java/org/sensorhub/impl/sensor/onvif/OnvifVideoOutputH264.java
+++ sensors/video/sensorhub-driver-onvif/src/main/java/org/sensorhub/impl/sensor/onvif/OnvifVideoOutputH264.java
@@ -1,6 +1,5 @@
 package org.sensorhub.impl.sensor.onvif;
 
-import org.sensorhub.api.sensor.SensorException;
 import org.sensorhub.impl.sensor.rtpcam.RTPVideoOutput;
 
 public class OnvifVideoOutputH264 extends RTPVideoOutput<OnvifCameraDriver> {
@@ -13,23 +12,7 @@ public class OnvifVideoOutputH264 extends RTPVideoOutput<OnvifCameraDriver> {
     }
 
 
-    //init will have the resolutions available and have options for the h264 stream based on camera specs
-    public void init(int imageWidth, int imageHeight) {
-        //implement the videoResolution call from the onvif profiles
 
-
-    }
-
-    public void start(OnvifBasicVideoConfig onvifBasicVideoConfig, OnvifRTSPConfig onvifRTSPConfig, int timeout) throws SensorException{
-        this.onvifBasicVideoConfig= onvifBasicVideoConfig;
-        this.onvifRTSPConfig= onvifRTSPConfig;
-
-
-        //OnvifCameraConfig cameraConfig = parentSensor.getConfiguration();
-      //super.start(onvifBasicVideoConfig, onvifRtspConfig, cameraConfig.timeout);
-
-
-    }
     public void stop(){
       super.stop();
     }
